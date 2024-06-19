b467de4df28e4ba9d764d5b3892b5598daf42ef7
Glenn <glennchiang24@gmail.com>
2024-06-19 20:52:27 +0800

    Implement teleportation of boid to other side of screen when it moves beyond screen boundaries

--- src/boid.ts
+++ src/boid.ts
@@ -10,12 +10,12 @@ export class Boid {
 
   private velocity: Velocity = { x: 1, y: -1 };
 
-  constructor(tipPoint: PointData) {
-    // Creates a boid sprite with its tip at the given tipPoint
+  constructor(tipPosition: PointData) {
+    // Creates a boid sprite with its tip at the given tip position
     const boidWidth = 16;
     const boidHeight = 20;
 
-    // The 3 vertices of the triangle. Positions are relative to the tip point
+    // The 3 vertices of the triangle. Positions are relative to the tip position
     const vertices: PointData[] = [
       { x: -boidWidth / 2, y: boidHeight },
       { x: 0, y: 0},
@@ -23,9 +23,7 @@ export class Boid {
     ];
 
     const sprite = new Graphics().poly(vertices).fill("white");
-    // The center and pivot of the boid is at its tip
-    sprite.x = tipPoint.x;
-    sprite.y = tipPoint.y;
+    sprite.position = tipPosition
 
     // Hit area of the sprite
     const hitArea = new Triangle(
@@ -46,9 +44,21 @@ export class Boid {
     container.addChild(this.sprite);
   }
 
+  // Called every frame
+  // Move the boid to its desired position in the next frame based on its velocity
+  // Rotate the boid toward its direction of movement
   move(deltaTime: number) {
     this.sprite.x += this.velocity.x * deltaTime
     this.sprite.y += this.velocity.y * deltaTime
     this.sprite.rotation = -Math.atan2(this.velocity.y, this.velocity.x)
+  }
+
+
+  get position(): PointData {
+    return this.sprite.position
+  }
+
+  setPosition(position: PointData) {
+    this.sprite.position = position
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,49 @@
-import { Application, Container, Graphics, PointData, Triangle } from "pixi.js";
+import {
+  Application,
+  Container,
+  Graphics,
+  PointData,
+  Rectangle,
+} from "pixi.js";
 import { Boid } from "./boid";
 
-(async () => {
-  const app = new Application();
-  await app.init({ resizeTo: window, background: "white", antialias: true });
-  document.body.appendChild(app.canvas);
+const app = new Application();
+await app.init({ resizeTo: window, background: "white", antialias: true });
+document.body.appendChild(app.canvas);
 
-  const containerWidth = 800;
-  const containerHeight = 640;
-  const container = new Container();
-  app.stage.addChild(container);
+// Main container in which boids will live
+const containerWidth = 800;
+const containerHeight = 640;
+const container = new Container();
+// Rectangle representing the container area
+const containerBounds = new Rectangle(0, 0, containerWidth, containerHeight);
+container.boundsArea = containerBounds;
+app.stage.addChild(container);
 
-  const background = new Graphics()
-    .rect(0, 0, containerWidth, containerHeight)
-    .fill("#5DADE2");
-  container.addChild(background);
+const background = new Graphics()
+  .rect(0, 0, containerWidth, containerHeight)
+  .fill("#5DADE2");
+container.addChild(background);
 
-  const boid = new Boid({
-    x: containerWidth / 2,
-    y: containerHeight / 2,
-  });
-  boid.spawn(container);
+const boid = new Boid({
+  x: containerWidth / 2,
+  y: containerHeight / 2,
+});
+boid.spawn(container);
 
-  const centreDot = new Graphics()
-    .circle(containerWidth / 2, containerHeight / 2, 4)
-    .fill("red");
-  container.addChild(centreDot);
+const centreDot = new Graphics()
+  .circle(containerWidth / 2, containerHeight / 2, 4)
+  .fill("red");
+container.addChild(centreDot);
 
-  app.ticker.add((ticker) => {
-    boid.move(ticker.deltaTime);
-  });
-})();
+app.ticker.add((ticker) => {
+  boid.move(ticker.deltaTime);
+
+  // If the boid moves beyond the container boundaries, teleport it to the other side of the container
+  if (!container.getBounds().containsPoint(boid.position.x, boid.position.y)) {
+    boid.setPosition({
+      x: container.getBounds().width - boid.position.x,
+      y: container.getBounds().height - boid.position.y,
+    });
+  }
+});
